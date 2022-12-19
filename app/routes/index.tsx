8f97e0c38d1736fa2b21be809f7bb316d2a00415
Saia Fonua <saiafonua@gmail.com>
2022-12-19 10:52:29 -0700

    add content and downlaod button resume

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,19 +8,27 @@ export default function Index() {
       <Header />
       <main className="flex flex-col items-center gap-y-4 px-4 justify-center md:flex-row-reverse">
         <img
-          className="max-w-xs rounded-lg "
+          className="max-w-xs rounded-full "
           alt="Saia Fonua"
-          src="/saia-fonua-picture.jpg"
+          src="/saia.jpeg"
         />
         <section className="flex flex-col gap-y-4">
-          <h1 className="text-6xl text-center">Saia Fonua</h1>
-          <h2 className="text-3xl text-center">About Me</h2>
-          <p className="text-xl max-w-sm">
+          <h1 className="text-6xl text-center">
+            Hi, I am Saia, FullStack Developer
+          </h1>
+          <p className="text-xl max-w-sm text-center">
             I currently work as a Software Engineer for The Church of Jesus
             Christ of Latter-day Saints. On my spare time, I like to spend time
             with family and friends, read books, learn Tongan, play basketball,
             exercise, serve the community, and work on side projects.
           </p>
+          <div className="mx-auto">
+            <a href="/Saia-Fonua-Resume.pdf" target="_blank">
+            <button className="bg-red-400 px-8 py-4 text-white text-lg" type="button">
+              Download Resume
+            </button>
+            </a>
+          </div>
         </section>
       </main>
       <Services />
