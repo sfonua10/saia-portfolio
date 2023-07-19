1619135820a61d9c6d585d0bdf94ac7c64af246a
Saia Fonua <saiafonua@gmail.com>
2023-07-19 00:21:21 -0600

    more content changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -251,7 +251,7 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Saia Fonua - Software designer, founder, and amateur astronaut
+          Saia Fonua - Software designer, learner, and amateur cook
         </title>
         <meta
           name="description"
@@ -261,7 +261,7 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Software designer, learner, and amateur cook.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Saia, a software designer and entrepreneur based in Lehi
